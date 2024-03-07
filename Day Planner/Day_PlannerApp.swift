0ffaf61ce399c54957b2b41bf3cc61fbc1db9577
Nam Do Hyun <brandonnam2020@gmail.com>
2024-03-07 15:31:27 +0800

    Initial Commit

---
+++ Day Planner/Day_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day_PlannerApp.swift
+//  Day Planner
+//
+//  Created by Nam Do Hyun on 2024/03/07.
+//
+
+import SwiftUI
+
+@main
+struct Day_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
